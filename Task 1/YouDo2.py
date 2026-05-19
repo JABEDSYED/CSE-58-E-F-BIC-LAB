417655fa9335c1284b713e567c61cdceeab41a27
JABEDSYED <141811952+JABEDSYED@users.noreply.github.com>
2026-05-19 16:07:19 +0600

    Create YouDo2.py

---
+++ Task 1/YouDo2.py
@@ -0,0 +1,8 @@
+movie_title = "Inception"
+release_year = 2010
+
+
+movie_info = f"The movie '{movie_title}' was released in {release_year}."
+
+print(movie_info)
+print("Type of release_year:", type(release_year))
